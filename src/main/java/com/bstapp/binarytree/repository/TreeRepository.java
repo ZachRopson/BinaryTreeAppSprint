10c195a08dcde6d47fffc7e0ac93c723c00715e8
Zachary Ropson <zachary.ropson@keyin.com>
2024-12-16 21:33:24 -0330

    Added gson dependency

--- src/main/java/com/bstapp/binarytree/repository/TreeRepository.java
+++ src/main/java/com/bstapp/binarytree/repository/TreeRepository.java
@@ -1,4 +1,8 @@
 package com.bstapp.binarytree.repository;
 
-public class TreeRepository {
-}
+import com.bstapp.binarytree.entity.TreeData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TreeRepository extends JpaRepository<TreeData, Long> {}
--- src/main/java/com/bstapp/binarytree/service/BinaryTreeService.java
+++ src/main/java/com/bstapp/binarytree/service/BinaryTreeService.java
@@ -1,4 +1,32 @@
 package com.bstapp.binarytree.service;
 
+import com.google.gson.Gson;
+import lombok.Data;
+
+@Data
 public class BinaryTreeService {
+    @Data
+    static class Node {
+        int value;
+        Node left, right;
+
+        Node(int value) { this.value = value; }
+    }
+
+    private Node root;
+
+    public void insert(int value) {
+        root = insertRec(root, value);
+    }
+
+    private Node insertRec(Node root, int value) {
+        if (root == null) return new Node(value);
+        if (value < root.value) root.left = insertRec(root.left, value);
+        else if (value > root.value) root.right = insertRec(root.right, value);
+        return root;
+    }
+
+    public String toJson() {
+        return new Gson().toJson(root);
+    }
 }
