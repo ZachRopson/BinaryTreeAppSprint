a67d1f2c18d4a975ef243f99076e9dfb0b22f973
Zachary Ropson <zachary.ropson@keyin.com>
2024-12-16 21:24:56 -0330

    Implemented BinaryTreeService for building and serializing binary search tree.

--- src/main/java/com/bstapp/binarytree/entity/TreeData.java
+++ src/main/java/com/bstapp/binarytree/entity/TreeData.java
@@ -1,4 +1,17 @@
 package com.bstapp.binarytree.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class TreeData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String inputNumbers;
+
+    @Lob
+    private String treeJson;
 }
